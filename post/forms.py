78011487dc1b3d0aa72659a1ca25dcfd8cd8d561
habitance <positipman@gmail.com>
2019-06-25 13:54:27 +0900

    before adding additional codes for forms.py

--- post/forms.py
+++ post/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Post
+from .models import *
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['image','text', 'location']
+        fields = ['image','text', 'city', 'location']
